b3e0619d959c6229be6c2d711860d5a4283f26b9
Rivan Jarjes <rivanjarjes@gmail.com>
2025-02-27 18:29:30 -0500

    arc-tools-web update

--- src/app/(tools)/arc-simulator/layout.tsx
+++ src/app/(tools)/arc-simulator/layout.tsx
@@ -1,31 +1,16 @@
 import "./globals.css";
-import type { Metadata } from "next";
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: "ARC Assembly Simulator",
-  description: "ARC Assembly Simulator by Rivan Jarjes"
-};
-
-export default function ArcSimulatorLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <head>
-        <style>{`
-          body {
-            margin: 0;
-            padding: 0;
-            background-color: #1A1A1A;
-          }
-        `}</style>
-      </head>
-      <body className={inter.className} style={{ margin: 0, padding: 0 }}>
+      <body className={inter.className}>
         {children}
       </body>
     </html>
